817660142c5c91be70d2b2e5137931e18dfbe429
tweet <tweet@users.noreply.github.com>
2026-07-02 15:39:31 +0800

    revert

--- script/version.ts
+++ script/version.ts
@@ -12,11 +12,11 @@ if (!Script.preview) {
   const body = await Bun.file(file)
     .text()
     .catch(() => "No notable changes")
-  // const dir = process.env.RUNNER_TEMP ?? "/tmp"
-  // const notesFile = `${dir}/opencode-release-notes.txt`
-  // await Bun.write(notesFile, body)
-  // await $`gh release create v${Script.version} -d --target ${sha} --title "v${Script.version}" --notes-file ${notesFile}`
-  // const release = await $`gh release view v${Script.version} --json tagName,databaseId`.json()
+  const dir = process.env.RUNNER_TEMP ?? "/tmp"
+  const notesFile = `${dir}/opencode-release-notes.txt`
+  await Bun.write(notesFile, body)
+  await $`gh release create v${Script.version} -d --target ${sha} --title "v${Script.version}" --notes-file ${notesFile}`
+  const release = await $`gh release view v${Script.version} --json tagName,databaseId`.json()
   const release = {};
   output.push(`release=${release.databaseId}`)
   output.push(`tag=${release.tagName}`)
