2929d5a934e5ccc16cf1251fc89921b81c944f4d
tweet <tweet@users.noreply.github.com>
2026-07-02 15:30:22 +0800

    add log

--- packages/opencode/script/build.ts
+++ packages/opencode/script/build.ts
@@ -237,6 +237,7 @@ if (Script.release) {
       await $`zip -r ../../${key}.zip *`.cwd(`dist/${key}/bin`)
     }
   }
+  console.log(`gh release upload v${Script.version} ./dist/*.zip ./dist/*.tar.gz --clobber --repo ${process.env.GH_REPO}`)
   await $`gh release upload v${Script.version} ./dist/*.zip ./dist/*.tar.gz --clobber --repo ${process.env.GH_REPO}`
 }
 
