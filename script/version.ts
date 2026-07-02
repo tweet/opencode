2d82cb8b634cf88f931e420ce1126127ab4688bb
tweet <tweet@users.noreply.github.com>
2026-07-02 14:51:34 +0800

    up

--- script/version.ts
+++ script/version.ts
@@ -7,7 +7,7 @@ const output = [`version=${Script.version}`]
 const sha = process.env.GITHUB_SHA ?? (await $`git rev-parse HEAD`.text()).trim()
 
 if (!Script.preview) {
-  await $`bun script/changelog.ts --to ${sha}`.cwd(process.cwd())
+  // await $`bun script/changelog.ts --to ${sha}`.cwd(process.cwd())
   const file = `${process.cwd()}/UPCOMING_CHANGELOG.md`
   const body = await Bun.file(file)
     .text()
